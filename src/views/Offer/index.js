7bc998059edb62df010061e3414d87a732496a83
chndt <chris@97ck.de>
2021-04-23 11:30:52 +0200

    Lay out basic views

---
+++ src/views/Offer/index.js
@@ -0,0 +1,22 @@
+import './style.css';
+
+export default function Offer(props) {
+  return (
+    <div className="tmbw-offer">
+      <div className="tmbw-offer-hero">
+      </div>
+
+      <div className="tmbw-offer-meta">
+      </div>
+
+      <div className="tmbw-offer-info">
+      </div>
+
+      <div className="tmbw-offer-upsell">
+      </div>
+
+      <div className="tmbw-offer-banner">
+      </div>
+    </div>
+  )
+}
---
+++ src/views/Overview/index.js
@@ -0,0 +1,42 @@
+import './style.css';
+
+export default function Overview(props) {
+  return (
+    <div className="tmbw-overview">
+      <div className="tmbw-overview-hero">
+        <div className="tmbw-overview-hero-inner">
+          <div className="tmbw-overview-hero-slider">
+          </div>
+
+          <div className="tmbw-overview-hero-title">
+            <div className="tmbw-overview-hero-title-inner">
+              <div className="tmbw-overview-hero-box">
+                <h5 className="tmbw-overview-hero-subline">
+                  Saison 2020/2021
+                </h5>
+                <h1 className="tmbw-overview-hero-headline">
+                  Entdecken Sie die Städte des Südens
+                </h1>
+                <p className="tmbw-overview-hero-body">
+                  Durch den Schwarzwald wandern und den Bodensee erleben, in Baden-Baden entspannen und in Heidelberg Kultur genießen, am Kaiserstuhl Wein verkosten oder das Donautal mit dem Rad erkunden: Es gibt mehr als einen Grund, in Deutschlands Süden zu reisen. In das Land mit den meisten Gastronomiesternen - und den meisten Sonnenstunden.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="tmbw-overview-filters">
+      </div>
+
+      <div className="tmbw-overview-results">
+      </div>
+
+      <div className="tmbw-overview-banner">
+      </div>
+
+      <div className="tmbw-overview-more-results">
+      </div>
+    </div>
+  )
+}
